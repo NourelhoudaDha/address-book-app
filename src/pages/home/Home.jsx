438550f7b80bc1afc4ef5e3f6044d27bfa04e54c
Nour <dhaouadi.nour.elhouda@gmail.com>
2021-03-06 00:25:19 +0100

    add navigate to settings

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useCallback, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { getUsers } from '../../store/actions'
@@ -11,6 +12,8 @@ import UserItem from '../../components/userItem/UserItem.jsx'
 import { MAX_LOADED_USERS } from '../../constants'
 
 const Home = () => {
+  const history = useHistory()
+
   const dispatch = useDispatch()
   const [searched, setSearched] = useState('')
   const users = useSelector(selectUsers)
@@ -33,13 +36,20 @@ const Home = () => {
     setSearched(event.target.value)
   }, [])
 
+  const memoizedGoToSettings = useCallback(() => {
+    history.push('/settings')
+  }, [])
+
   return (
     <section className='px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4'>
       <header className='flex items-center justify-between'>
         <h2 className='text-lg leading-6 font-medium text-black'>
           Address Book
         </h2>
-        <button className='hover:bg-light-blue-200 hover:text-light-blue-800 group flex items-center rounded-md bg-light-blue-100 text-light-blue-600 text-sm font-medium px-4 py-2'>
+        <button
+          className='hover:bg-light-blue-200 hover:text-light-blue-800 group flex items-center rounded-md bg-light-blue-100 text-light-blue-600 text-sm font-medium px-4 py-2'
+          onClick={memoizedGoToSettings}
+        >
           <svg
             className='group-hover:text-light-blue-600 text-light-blue-500 mr-2'
             width='20'
