afd3667a330d076ad945ce5d91916ea3d60657c9
Nour <dhaouadi.nour.elhouda@gmail.com>
2021-03-05 14:08:34 +0100

    add tailwindcss to the app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import store from './store/store'
 import { Provider } from 'react-redux'
+import './index.scss'
 
 ReactDOM.render(
   <Provider store={store}>
